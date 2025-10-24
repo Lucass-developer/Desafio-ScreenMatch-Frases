5841e4e48433e908a7ee92074ed63a47110196d2
Lucas Silva <lsilva.kingone@gmail.com>
2025-10-24 13:40:09 -0300

    "Finalizando desafiio"

--- src/main/java/br/com/lucassdev/ScreenMatchFrases/config/CorsConfiguration.java
+++ src/main/java/br/com/lucassdev/ScreenMatchFrases/config/CorsConfiguration.java
@@ -4,10 +4,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import io.micrometer.common.lang.NonNull;
+
 @Configuration
 public class CorsConfiguration implements WebMvcConfigurer {
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
+    public void addCorsMappings(@NonNull CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://127.0.0.1:5500")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
