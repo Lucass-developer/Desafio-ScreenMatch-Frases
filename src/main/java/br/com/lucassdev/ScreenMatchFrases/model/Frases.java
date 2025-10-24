4a132a4a4f75fb8fc94f5f225f7cfb2ac99e00b7
Lucas Silva <lsilva.kingone@gmail.com>
2025-10-24 12:10:12 -0300

    Configurando e populando banco de dados

--- src/main/java/br/com/lucassdev/ScreenMatchFrases/model/Frases.java
+++ src/main/java/br/com/lucassdev/ScreenMatchFrases/model/Frases.java
@@ -1,8 +1,17 @@
 package br.com.lucassdev.ScreenMatchFrases.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Frases {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String frase;
+    private String personagem;
+    private String titulo;
+    private String poster;
 }
